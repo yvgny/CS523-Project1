4cd8852319e74340dfc0bc398791adba6e5b0eb7
Sacha Kozma <sacha.kozma@gmail.com>
2020-02-28 16:24:41 +0100

    Finished mpc test

--- mpc_test.go
+++ mpc_test.go
@@ -11,7 +11,7 @@ func TestMPCProtocol(t *testing.T) {
 
 		N := uint64(len(testCircuit.Peers))
 		P := make([]*LocalParty, N, N)
-		dummyProtocol := make([]*Protocol, N, N)
+		protocol := make([]*Protocol, N, N)
 
 		var err error
 		wg := new(sync.WaitGroup)
@@ -20,24 +20,23 @@ func TestMPCProtocol(t *testing.T) {
 			P[i].WaitGroup = wg
 			check(err)
 
-			// TODO Finish test from here
-			dummyProtocol[i] = P[i].NewProtocol(testCircuit.Inputs[i][i], testCircuit.Circuit)
+			protocol[i] = P[i].NewProtocol(testCircuit.Inputs[i][GateID(i)], testCircuit.Circuit)
 		}
 
 		network := GetTestingTCPNetwork(P)
 		fmt.Println("parties connected")
 
-		for i, Pi := range dummyProtocol {
+		for i, Pi := range protocol {
 			Pi.BindNetwork(network[i])
 		}
 
-		for _, p := range dummyProtocol {
+		for _, p := range protocol {
 			p.Add(1)
 			go p.Run()
 		}
 		wg.Wait()
 
-		for _, p := range dummyProtocol {
+		for _, p := range protocol {
 			fmt.Println(p, "completed with output", p.Output)
 		}
 
