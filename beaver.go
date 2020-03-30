94973707c8c970442130efc7d89fc697edea5b1a
Ricardo Ferreira Ribeiro <ricardo.ferreiraribeiro@protonmail.com>
2020-03-30 11:13:45 +0200

    Start implementing protocol

--- beaver.go
+++ beaver.go
@@ -9,13 +9,22 @@ import (
 )
 
 type BeaverMessage struct {
-	Value []uint64
+	Size  int
+	Value []byte
 }
 
 type BeaverProtocol struct {
 	*LocalParty
-	Params *bfv.Parameters
-	Peers  map[PartyID]*BeaverRemoteParty
+	Params         *bfv.Parameters
+	Peers          map[PartyID]*BeaverRemoteParty
+	Encoder        bfv.Encoder
+	Evaluator bfv.Evaluator
+	BeaverTriplets map[PartyID]Triplet
+}
+
+type Triplet struct {
+	b *bfv.Plaintext
+	c []uint64
 }
 
 type BeaverRemoteParty struct {
@@ -29,6 +38,9 @@ func (lp *LocalParty) NewBeaverProtocol(params *bfv.Parameters) *BeaverProtocol 
 	cep.LocalParty = lp
 	cep.Peers = make(map[PartyID]*BeaverRemoteParty, len(lp.Peers))
 	cep.Params = params
+	cep.Encoder = bfv.NewEncoder(params)
+	cep.Evaluator = bfv.NewEvaluator(params)
+	cep.BeaverTriplets = make(map[PartyID]Triplet, len(lp.Peers))
 	for i, rp := range lp.Peers {
 		cep.Peers[i] = &BeaverRemoteParty{
 			RemoteParty: rp,
@@ -52,17 +64,16 @@ func (cep *BeaverProtocol) BindNetwork(nw *TCPNetworkStruct) {
 		// Receiving loop from remote
 		go func(conn net.Conn, rp *BeaverRemoteParty) {
 			for {
-				var val uint64
-				var out WireID
+				var size int
+
 				var err error
+				err = binary.Read(conn, binary.BigEndian, &size)
+				check(err)
+				val := make([]byte, size)
 				err = binary.Read(conn, binary.BigEndian, &val)
 				check(err)
-				err = binary.Read(conn, binary.BigEndian, &out)
-				check(err)
-				msg := BeaverMessage{
-					Value: val,
-					Out:   out,
-				}
+
+				msg := BeaverMessage{Size:size, Value:val}
 				//fmt.Println(cep, "receiving", msg, "from", rp)
 				rp.ReceiveChan <- msg
 			}
@@ -75,8 +86,8 @@ func (cep *BeaverProtocol) BindNetwork(nw *TCPNetworkStruct) {
 			for open {
 				m, open = <-rp.Chan
 				//fmt.Println(cep, "sending", m, "to", rp)
+				check(binary.Write(conn, binary.BigEndian, m.Size))
 				check(binary.Write(conn, binary.BigEndian, m.Value))
-				check(binary.Write(conn, binary.BigEndian, m.Out))
 			}
 		}(conn, rp)
 	}
@@ -88,8 +99,71 @@ func (cep *BeaverProtocol) Run() {
 
 	// TODO
 
+	cep.GenerateTriplets()
+
+	bi := cep.BeaverTriplets[cep.ID].b
+	for id, peer := range cep.Peers {
+		if id != cep.ID {
+			msg := <- peer.ReceiveChan
+			dj := bfv.NewCiphertext(cep.Params, 1)
+			err := dj.UnmarshalBinary(msg.Value)
+			check(err)
+
+			rij := newRandomVec(1<<cep.Params.LogN, cep.Params.T)
+
+			cep.BeaverTriplets[cep.ID] = Triplet{
+				b: cep.BeaverTriplets[cep.ID].b,
+				c: subVec(cep.BeaverTriplets[cep.ID].c, rij, cep.Params.T),
+			}
+
+			rijPt := bfv.NewPlaintext(cep.Params)
+			cep.Encoder.EncodeUint(rij, rijPt)
+
+			mul := bfv.NewCiphertext(cep.Params, 1)
+			cep.Evaluator.Mul(dj, bi, mul)
+
+			//TODO: errors (ligne 19 et 20 protocol)
+			dij := bfv.NewCiphertext(cep.Params, 1)
+			cep.Evaluator.Add(mul, rijPt, dij)
+
+
+		}
+	}
+
 	if cep.WaitGroup != nil {
 		cep.WaitGroup.Done()
 	}
+
+}
+
+func (cep *BeaverProtocol) GenerateTriplets() {
+	keyGen := bfv.NewKeyGenerator(cep.Params)
+
+	ai := newRandomVec(1<<cep.Params.LogN, cep.Params.T)
+	bi := newRandomVec(1<<cep.Params.LogN, cep.Params.T)
+	ci := mulVec(ai, bi, cep.Params.T)
+
+	aiPt := bfv.NewPlaintext(cep.Params)
+	cep.Encoder.EncodeUint(ai, aiPt)
+
+	biPt := bfv.NewPlaintext(cep.Params)
+	cep.Encoder.EncodeUint(bi, biPt)
+
+	ski := keyGen.GenSecretKey()
+
+	cep.BeaverTriplets[cep.ID] = Triplet{b: biPt, c: ci}
+
+	encryptor := bfv.NewEncryptorFromSk(cep.Params, ski)
+	di := encryptor.EncryptNew(aiPt)
+	bytes, err := di.MarshalBinary()
+	check(err)
+	beaverMessage := BeaverMessage{Size: len(bytes), Value: bytes}
+
+	for id, peer := range cep.Peers {
+		if id != cep.ID {
+			peer.Chan <- beaverMessage
+		}
+	}
+
 
 }
